d143d1a0a3f5e167eee5b1a62a2ec1428cf2a527
pabloquihui <pabloquihui@gmail.com>
2023-01-30 17:06:11 -0600

    fix metrics

--- train.py
+++ train.py
@@ -82,8 +82,9 @@ lossfn = keras.losses.categorical_crossentropy
 metrics = [
             sm.metrics.IOUScore(threshold=0.5),
             sm.metrics.FScore(threshold=0.5),
-            tfa.metrics.F1Score(num_classes=5, threshold=None, average='macro'),
-            tfa.metrics.F1Score(num_classes=5, threshold=None, average='weighted')]
+            tfa.metrics.F1Score(num_classes=5, threshold=None, average='macro', name = 'f1_macro'),
+            tfa.metrics.F1Score(num_classes=5, threshold=None, average='weighted', name = 'f1_weighted'),
+            tfa.metrics.F1Score(num_classes=5, threshold=None, average='micro', name = 'f1_micro')]
 
 
 # UNET Montecarlo Dropout
