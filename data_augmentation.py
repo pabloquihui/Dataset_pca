d0266811332bbce7e976c1fadabf4d91c00762f6
pabloquihui <pabloquihui@gmail.com>
2023-01-31 11:45:12 -0600

    x

--- data_augmentation.py
+++ data_augmentation.py
@@ -44,13 +44,13 @@ def process(data):
 @tf.function
 def random_bright(image_mask, seed):
     image, mask = image_mask
-    image = tf.image.stateless_random_brightness(image, max_delta=0.6, seed=seed)
+    image = tf.image.stateless_random_brightness(image, max_delta=0.5, seed=seed)
     return image, mask
 
 @tf.function
 def random_contrast(image_mask, seed):
     image, mask = image_mask
-    image = tf.image.stateless_random_contrast(image, lower=0.3, upper=1, seed=seed)
+    image = tf.image.stateless_random_contrast(image, lower=0.4, upper=1, seed=seed)
     return image, mask
 
 @tf.function
@@ -114,7 +114,7 @@ def random_central_crop(image_mask, seed):
 def augment(image_mask, seed):
     image, mask = image_mask
     # new_seed = tf.random.experimental.stateless_split(seed, num=1)[0][0]
-    image, mask = random_central_crop((image, mask), seed)
+    # image, mask = random_central_crop((image, mask), seed)
     image, mask = random_bright((image, mask), seed)
     image, mask = random_contrast((image, mask), seed)
     # image, mask = random_flip((image, mask), seed)
