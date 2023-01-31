87477ad18cbd87ed8f1c885b34d50e42d73ae1f4
pabloquihui <pabloquihui@gmail.com>
2023-01-31 11:39:56 -0600

    x

--- data_augmentation.py
+++ data_augmentation.py
@@ -105,8 +105,8 @@ def random_central_crop(image_mask, seed):
     central_fraction = tf.random.stateless_uniform([1], seed, minval=min_fraction, maxval=max_fraction)
     image = tf.image.central_crop(image, central_fraction[0])
     mask = tf.image.central_crop(mask, central_fraction[0])
-    image = tf.image.resize(image, [256,256, 1])
-    mask = tf.image.resize(mask, [256, 256, 5])
+    image = tf.image.resize(image, [256,256])
+    mask = tf.image.resize(mask, [256, 256])
     return image, mask
 
 # ### Augmentation
