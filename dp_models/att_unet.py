f823ee39468d2879d2f60e3ce53419d17f15fcb7
pabloquihui <pabloquihui@gmail.com>
2023-02-01 19:13:00 -0600

    run models uncertainty

--- dp_models/att_unet.py
+++ dp_models/att_unet.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import Conv2D, UpSampling2D, Activation, Input, Dro
 from tensorflow.keras.models import Model
 from keras.layers.core import Lambda
 import tensorflow.keras.backend as K
+from dp_models.mcdropout import MCDropout
 
 
 def attention_up_and_concate(down_layer, layer, data_format='channels_last'):
@@ -88,6 +89,67 @@ def attention_unet_model(n_classes=5, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNEL
     c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
     c9 = Dropout(0.1)(c9)
     c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+    model = Model(inputs=[inputs], outputs=[outputs])
+
+    # NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    # model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # model.summary()
+
+    return model
+
+def mc_attention_unet_model(n_classes=5, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
+    # Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    # s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
+
+    # Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = MCDropout(0.1)(p1)
+
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = MCDropout(0.1)(p2)
+
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+    p3 = MCDropout(0.2)(p3)
+
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+    p4 = MCDropout(0.2)(p4)
+
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    c5 = MCDropout(0.3)(c5)
+    # Expansive path
+    u6 = attention_up_and_concate(c5, c4)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    c6 = MCDropout(0.2)(c6)
+
+    u7 = attention_up_and_concate(c6, c3)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    c7 = MCDropout(0.2)(c7)
+
+    u8 = attention_up_and_concate(c7, c2)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+    c8 = MCDropout(0.1)(c8)
+
+    u9 = attention_up_and_concate(c8, c1)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+    c9 = MCDropout(0.1)(c9)
 
     outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
     model = Model(inputs=[inputs], outputs=[outputs])
--- train4test.py
+++ train4test.py
@@ -19,7 +19,7 @@ from dp_models.unet_MC import multi_unet_model as mc_unet_model
 from dp_models.unet import unet_model
 from dp_models.Dense_UNet import mc_dense_unet, dense_unet
 from dp_models.att_fpa import att_unet
-from dp_models.att_unet import attention_unet_model
+from dp_models.att_unet import attention_unet_model, mc_attention_unet_model
 import tensorflow_addons as tfa
 from data_augmentation import augment, preprocess
 import wandb
@@ -51,7 +51,7 @@ else:
     aug = 'Orig'
 
 def main(train):
-    run = wandb.init(reinit=True, entity='cv_inside', project='Prostate_Ablation', name=f'AttUNET_{aug}_final')
+    
     tf.keras.backend.clear_session()
 
     LR = 0.0001
@@ -75,54 +75,61 @@ def main(train):
                     )
     else:
         train_ds = train_ds.shuffle(1000).batch(BATCH_SIZE)
-    # UNET
-    # model = unet_model(n_classes=N_CLASSES, IMG_HEIGHT=IMG_H, IMG_WIDTH=IMG_W, IMG_CHANNELS=IMG_CH)
-    # model.compile(loss=lossfn, optimizer=optim, metrics = metrics)
-    # name = 'unet'
-    # folder = 'UNET'
+    
+    def get_model(name):
+        if name == 'unet':
+            return mc_unet_model(n_classes=N_CLASSES, IMG_HEIGHT=IMG_H, IMG_WIDTH=IMG_W, IMG_CHANNELS=IMG_CH)
+        elif name == 'att_unet':
+            return mc_attention_unet_model(n_classes=N_CLASSES, IMG_HEIGHT=IMG_H, IMG_WIDTH=IMG_W, IMG_CHANNELS=IMG_CH)
+        elif name == 'dense_unet':
+            return mc_dense_unet(input_shape=(256, 256, 1), num_classes=5)
+        elif name == 'att_dense_unet':
+            return mc_attn_dense_unet(input_shape=(256, 256, 1), num_classes=5)
 
-    # ATTN UNET 
-    model = att_unet_org(img_h=IMG_H, img_w=IMG_W, img_ch=IMG_CH, n_label=N_CLASSES, data_format='channels_last')
-    model = attention_unet_model(n_classes=N_CLASSES, IMG_HEIGHT=IMG_H, IMG_WIDTH=IMG_W, IMG_CHANNELS=IMG_CH)
-    model.compile(loss=lossfn, optimizer=optim, metrics = metrics)
-    name = 'attn_unet'
-    folder = 'ATTN_UNET'
-
+    names = np.array(['unet', 'att_unet', 'dense_unet', 'att_dense_unet'])
+    folder = 'Uncertainty_comparison'
     if not os.path.exists(folder):
-        os.makedirs(folder)
+            os.makedirs(folder)
 
-    # Callbacks
-    wandb_callback = WandbCallback(
-                                    monitor='val_loss',
-                                    mode='min',
-                                    save_model=False,
-                                    save_weights_only=False
-                                    )
+    scores_metrics = []
+    for model_name in names:
+        run = wandb.init(reinit=True, entity='cv_inside', project='Prostate_Ablation', name=f'{model_name}_{aug}_final')
+        model = get_model(model_name)
+        model.compile(loss=lossfn, optimizer=optim, metrics = metrics)
     
-    callbacks= [wandb_callback]
+        # Callbacks
+        wandb_callback = WandbCallback(
+                                        monitor='val_loss',
+                                        mode='min',
+                                        save_model=False,
+                                        save_weights_only=False
+                                        )
+        
+        callbacks= [wandb_callback]
 
-    #Training
-    history = model.fit(
-        train_ds, 
-        epochs=EPOCHS, 
-        callbacks=callbacks,
-        )
+        #Training
+        history = model.fit(
+            train_ds, 
+            epochs=EPOCHS, 
+            callbacks=callbacks,
+            )
 
-    scores = model.evaluate(test, verbose=0)
-    print(f'Scores for test set: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]};      {model.metrics_names[2]} of {scores[2]}')
+        scores = model.evaluate(test, verbose=0)
+        scores_metrics.append(scores)
+        print(f'Scores for test set: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]};      {model.metrics_names[2]} of {scores[2]}')
 
-    # serialize model to json
-    json_model = model.to_json()#save the model architecture to JSON file
-    with open(f'{folder}/{name}_{EPOCHS}_{aug}_final.json', 'w') as json_file:
-        json_file.write(json_model)
-    model.save_weights(f'{folder}/{name}_{EPOCHS}_{aug}_final.h5')
-    run.finish()
-    
+        # serialize model to json
+        json_model = model.to_json()#save the model architecture to JSON file
+        with open(f'{folder}/{model_name}_{EPOCHS}_{aug}_final.json', 'w') as json_file:
+            json_file.write(json_model)
+        model.save_weights(f'{folder}/{model_name}_{EPOCHS}_{aug}_final.h5')
+        run.finish()
+    np.save('Uncertainty_comparison/', scores_metrics)
     return 
 
 if __name__ == "__main__":
     print ("Executing training")
-    scores = main(train)
+    main(train)
 ### Send notification to iPhone ###
     notif = requests.post(url=url_notif)
 
