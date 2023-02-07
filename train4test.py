0d8aab496c2601d3a1b40d70940709f056027fc8
pabloquihui <pabloquihui@gmail.com>
2023-02-06 23:33:50 -0600

    more epochs

--- train4test.py
+++ train4test.py
@@ -51,7 +51,7 @@ def get_parameters():
     metrics = [
             sm.metrics.IOUScore(threshold=0.5),
             sm.metrics.FScore(threshold=0.5),]
-    EPOCHS = 140
+    EPOCHS = 170
     return EPOCHS, optim, lossfn, metrics
 
 def get_augmentation():
